4d59f99c5509225566b1a28ad2b6786359ee15c3
mounika145 <78247030+mounika145@users.noreply.github.com>
2022-02-17 15:12:06 +0530

    Add files via upload

---
+++ facebook-clone/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import LoginPage from './LoginPage/LoginPage';
+import HeaderArea from './HeaderArea/HeaderArea';
+import {Switch,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/LoginPage"exact={true}component={LoginPage}/>
+        <Route path="/HeaderArea"component={HeaderArea}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
